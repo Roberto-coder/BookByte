5bd7b782b3a29af10bfe740b135b3ecdb8c991c7
Josue Nájera <najera.jimenez.josue@gmail.com>
2024-04-03 18:29:18 -0600

    enviate

--- rutas/pruebas-ruta.js
+++ rutas/pruebas-ruta.js
@@ -4,8 +4,5 @@ import bcrypt from "bcrypt";
 import pool from '../config/database.js';
 import path from 'path';
 const router = express.Router();
-router.get('/header',(req,res)=>{
-    const error_msg = req.flash('error'); // Obtiene el mensaje de error
-    res.render('header');
-});
+
 export default router;
