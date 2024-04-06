bcdd3554839deee228101f694a82079a915b20dc
BrayJAN <braynanj123@gmail.com>
2024-04-05 19:08:15 -0600

    holaa

--- rutas/index-ruta.js
+++ rutas/index-ruta.js
@@ -2,9 +2,14 @@ import  express  from "express";
 import pool from '../config/database.js';
 const router = express.Router();
 router.get('/', (req, res) =>{
-    pool.query('SELECT * FROM books ORDER BY book_datePublication DESC LIMIT 10',
+    pool.query('SELECT * FROM books ORDER BY book_datePublication DESC LIMIT 4',
     (error, results) =>{
+        if (error) {
+            res.render("ERROR");
+        }else{
+        console.log(results);
         res.render("index", { newBooks: results });
+        }
     });
 
 });
